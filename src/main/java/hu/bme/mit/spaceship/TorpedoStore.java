5b222db37b1429c32c35429415a1581903332e38
JozsefN <jozsefa2015@gmail.com>
2024-10-09 16:30:13 +0200

    randomgenerator changed to Private

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -12,7 +12,7 @@ public class TorpedoStore {
   // rate of failing to fire torpedos [0.0, 1.0]
   private double FAILURE_RATE = 0.0; //NOSONAR
 
-  Random generator = new Random(); //Reusable Random
+  private Random generator = new Random(); //Reusable Random
 
   private int torpedoCount = 0;
 
